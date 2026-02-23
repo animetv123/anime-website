66bdd2b77ca3c48418180df454b479dc079a6c28
animetv123 <samyarhosienpoor@gmail.com>
2026-02-23 15:44:10 +0330

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,16 @@
+const animeData = [
+  {
+    title: "Demon Slayer",
+    year: 2019,
+    genre: "Action, Fantasy",
+    poster: "images/sample1.jpg",
+    summary: "Tanjiro fights demons to save his sister."
+  },
+  {
+    title: "One Piece",
+    year: 1999,
+    genre: "Adventure",
+    poster: "images/sample1.jpg",
+    summary: "Luffy searches for the legendary treasure."
+  }
+];
